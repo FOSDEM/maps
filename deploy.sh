b2f2e57b02cfa5fd265fa8ba23dec960f75e4359
Louis Taylor <louis@kragniz.eu>
2017-01-18 19:18:15 +0000

    Update deploy script to update packages

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,23 @@ set -ex
 
 # for use on nav.fosdem.org
 
+# FIXME: make this a function and remove this duplication
 cd ~/data/map/maps
 git remote update
 git reset --hard origin/master
+
+cd "~/data/map/maps-2017"
+git remote update
+git reset --hard origin/master
+
+cd "~/data/map/maps-2017-sat"
+git remote update
+git reset --hard origin/master
+
+cd "~/data/map/maps-2017-sun"
+git remote update
+git reset --hard origin/master
+
 
 cd ~/c3nav
 source env/bin/activate
