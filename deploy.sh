67a93d77e10e0e234b8114b7507e724f8d0e50e4
Louis Taylor <louis@kragniz.eu>
2017-01-31 20:31:22 +0000

    Update deploy.sh for single map package

--- deploy.sh
+++ deploy.sh
@@ -4,28 +4,9 @@ set -ex
 
 # for use on nav.fosdem.org
 
-# FIXME: make this a function and remove this duplication
 cd ~/data/map/maps
 git remote update
 git reset --hard origin/master
 
-cd ~/data/map/maps-2017
-git remote update
-git reset --hard origin/master
-
-cd ~/data/map/maps-2017-sat
-git remote update
-git reset --hard origin/master
-
-cd ~/data/map/maps-2017-sun
-git remote update
-git reset --hard origin/master
-
-
-cd ~/c3nav
-source env/bin/activate
-cd src
-python ./manage.py migrate
-python ./manage.py loadmap -y
-python ./manage.py rendermap
-python ./manage.py buildgraph
+cd ~/data
+sudo docker run --rm --name fosdem-map -v $(pwd):/data -v $(pwd)/c3nav.cfg:/etc/c3nav/c3nav.cfg c3nav/c3nav load_build
